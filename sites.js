3862e4bab3f354a30b078c4764f0e2338ab4065c
SakuraNiroku <liangyin17@outlook.com>
2024-05-04 23:08:15 +0800

    init

---
+++ sites.js
@@ -0,0 +1,10 @@
+starsite = {
+    "室外ling源":"https://www.mc-lingyuan.fun",
+    "室外ling源服务器监测":"https://lys.catver.top",
+    "3rdMusicSite":"https://ui163.qxiao.eu.org"
+}
+
+site = {
+    "百度":"https://baidu.com",
+    "Bing":"https://bing.com"
+}
