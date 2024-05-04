7fab406f371dc7a57a12e4e3b2422e756a907481
SakuraNiroku <liangyin17@outlook.com>
2024-05-04 23:22:55 +0800

    fix

--- sites.js
+++ sites.js
@@ -1,10 +1,10 @@
 starsite = {
-    "室外ling源":"https://www.mc-lingyuan.fun",
-    "室外ling源服务器监测":"https://lys.catver.top",
+    "世外ling源":"https://www.mc-lingyuan.fun",
+    "世外ling源服务器监测":"https://lys.catver.top",
     "3rdMusicSite":"https://ui163.qxiao.eu.org"
 }
 
 site = {
     "百度":"https://baidu.com",
-    "Bing":"https://bing.com"
+    "bing":"https://bing.com"
 }
